423f7747c1cde1a5ccf9ec97e9efde2749f2f325
Anıl Konaç <anil.konac@gmail.com>
2022-01-10 23:51:56 +0300

    Implement snake movement
    - Revise Snake and Unit types
    - Add a Snake constructor that creates units of the snake
    - Add updating unit positions

--- snake.go
+++ snake.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 const (
 	directionUp uint8 = iota
 	directionDown
@@ -8,15 +10,56 @@ const (
 	directionTotal
 )
 
-type unit struct {
-	posX      float32
-	posY      float32
+const unitLength = 20
+
+type Unit struct {
+	posX      float64
+	posY      float64
 	direction uint8
-	frontUnit *unit
-	backUnit  *unit
 }
 
-type snake struct {
+type Snake struct {
 	speed uint8
-	head  *unit
+	units []Unit
+}
+
+func newSnake(posX float64, posY float64, direction uint8, speed uint8, length uint8) *Snake {
+	if direction >= directionTotal {
+		panic("direction parameter is invalid.")
+	}
+	if posX > screenWidth {
+		panic("initial x position of the snake is out of screen.")
+	}
+	if posY > screenHeight {
+		panic("initial x position of the snake is out of screen.")
+	}
+
+	snake := &Snake{speed: speed}
+
+	// Generate units of the snake
+	snake.units = make([]Unit, length, math.MaxUint8)
+	iLength := int(length)
+	for i := 0; i < iLength; i++ {
+		curUnit := &snake.units[i]
+		curUnit.direction = direction
+
+		// Compute position of current unit
+		distanceToHead := float64(i * unitLength)
+		switch direction {
+		case directionUp:
+			curUnit.posX = posX
+			curUnit.posY = posY - distanceToHead
+		case directionDown:
+			curUnit.posX = posX
+			curUnit.posY = posY + distanceToHead
+		case directionRight:
+			curUnit.posX = posX - distanceToHead
+			curUnit.posY = posY
+		case directionLeft:
+			curUnit.posX = posX + distanceToHead
+			curUnit.posY = posY
+		}
+	}
+
+	return snake
 }
