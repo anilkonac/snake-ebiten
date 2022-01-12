d3d3cd6320f89ee23261a039604a63d43aaf51ba
Anıl Konaç <anil.konac@gmail.com>
2022-01-12 15:13:43 +0300

    wip: teleport to the other side

--- game.go
+++ game.go
@@ -9,22 +9,22 @@ import (
 )
 
 const (
-	snakeHeadPosX = 200
-	snakeHeadPosY = 200
-	snakeSpeed    = 200
-	snakeLength   = 5
-	deltaTime     = 1.0 / 60.0
+	snakeHeadCenterX = screenWidth - 15
+	snakeHeadCenterY = screenHeight / 2.0
+	snakeSpeed       = 1
+	snakeLength      = 5
+	deltaTime        = 1.0 / 60.0
 )
 
 // Game implements ebiten.Game interface.
 type Game struct {
 	tps   float64
-	snake *Snake
+	snake *snake
 }
 
 func newGame() *Game {
 	game := new(Game)
-	game.snake = newSnake(snakeHeadPosX, snakeHeadPosY, directionRight, snakeSpeed, snakeLength)
+	game.snake = newSnake(snakeHeadCenterX, snakeHeadCenterY, directionRight, snakeSpeed, snakeLength)
 
 	return game
 }
@@ -33,22 +33,7 @@ func newGame() *Game {
 func (g *Game) Update() error {
 	g.tps = ebiten.CurrentTPS()
 
-	// Update snake position
-	for indexUnit := 0; indexUnit < len(g.snake.units); indexUnit++ {
-		curUnit := &g.snake.units[indexUnit]
-
-		travelDistance := float64(g.snake.speed) * deltaTime
-		switch curUnit.direction {
-		case directionRight:
-			curUnit.posX += travelDistance
-		case directionLeft:
-			curUnit.posX -= travelDistance
-		case directionUp:
-			curUnit.posY -= travelDistance
-		case directionDown:
-			curUnit.posY += travelDistance
-		}
-	}
+	g.snake.update()
 
 	return nil
 }
@@ -57,6 +42,8 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	// Print TPS
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %.1f", g.tps))
+	head := &g.snake.units[0]
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Head X: %.2f Y: %.2f", head.posX, head.posY), 0, 15)
 
 	// Draw snake
 	for indexUnit := 0; indexUnit < len(g.snake.units); indexUnit++ {
@@ -72,7 +59,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		}
 
 		// Draw unit
-		ebitenutil.DrawRect(screen, curUnit.posX, curUnit.posY, unitLength, unitLength, unitColor)
+		ebitenutil.DrawRect(screen, curUnit.posX-centerOffset, curUnit.posY-centerOffset, unitLength, unitLength, unitColor)
 	}
 }
 
--- snake.go
+++ snake.go
@@ -10,34 +10,37 @@ const (
 	directionTotal
 )
 
-const unitLength = 20
+const (
+	unitLength   = 20
+	centerOffset = unitLength / 2.0
+)
 
-type Unit struct {
+type unit struct {
 	posX      float64
 	posY      float64
 	direction uint8
 }
 
-type Snake struct {
+type snake struct {
 	speed uint8
-	units []Unit
+	units []unit
 }
 
-func newSnake(posX float64, posY float64, direction uint8, speed uint8, length uint8) *Snake {
+func newSnake(centerX float64, centerY float64, direction uint8, speed uint8, length uint8) *snake {
 	if direction >= directionTotal {
 		panic("direction parameter is invalid.")
 	}
-	if posX > screenWidth {
+	if centerX > screenWidth {
 		panic("initial x position of the snake is out of screen.")
 	}
-	if posY > screenHeight {
+	if centerY > screenHeight {
 		panic("initial x position of the snake is out of screen.")
 	}
 
-	snake := &Snake{speed: speed}
+	snake := &snake{speed: speed}
 
 	// Generate units of the snake
-	snake.units = make([]Unit, length, math.MaxUint8)
+	snake.units = make([]unit, length, math.MaxUint8)
 	for i := uint8(0); i < length; i++ {
 		curUnit := &snake.units[i]
 		curUnit.direction = direction
@@ -46,19 +49,66 @@ func newSnake(posX float64, posY float64, direction uint8, speed uint8, length u
 		distanceToHead := float64(i) * unitLength
 		switch direction {
 		case directionUp:
-			curUnit.posX = posX
-			curUnit.posY = posY - distanceToHead
+			curUnit.posX = centerX
+			curUnit.posY = centerY - distanceToHead
 		case directionDown:
-			curUnit.posX = posX
-			curUnit.posY = posY + distanceToHead
+			curUnit.posX = centerX
+			curUnit.posY = centerY + distanceToHead
 		case directionRight:
-			curUnit.posX = posX - distanceToHead
-			curUnit.posY = posY
+			curUnit.posX = centerX - distanceToHead
+			curUnit.posY = centerY
 		case directionLeft:
-			curUnit.posX = posX + distanceToHead
-			curUnit.posY = posY
+			curUnit.posX = centerX + distanceToHead
+			curUnit.posY = centerY
 		}
 	}
 
 	return snake
+}
+
+func (s *snake) update() {
+	// Update snake position
+	for indexUnit := 0; indexUnit < len(s.units); indexUnit++ {
+		curUnit := &s.units[indexUnit]
+
+		travelDistance := float64(s.speed) * deltaTime
+		switch curUnit.direction {
+		case directionRight:
+			curUnit.moveRight(travelDistance)
+		case directionLeft:
+			curUnit.moveLeft(travelDistance)
+		case directionUp:
+			curUnit.moveUp(travelDistance)
+		case directionDown:
+			curUnit.moveDown(travelDistance)
+		}
+	}
+}
+
+func (u *unit) moveUp(dist float64) {
+	u.posY -= dist
+	if u.posY < 0 {
+		u.posY += screenWidth
+	}
+}
+
+func (u *unit) moveDown(dist float64) {
+	u.posY += dist
+	if u.posY > screenHeight {
+		u.posY -= screenWidth
+	}
+}
+
+func (u *unit) moveLeft(dist float64) {
+	u.posX -= dist
+	if u.posX < 0 {
+		u.posX += screenWidth
+	}
+}
+
+func (u *unit) moveRight(dist float64) {
+	u.posX += dist
+	if u.posX > screenWidth {
+		u.posX -= screenWidth
+	}
 }
