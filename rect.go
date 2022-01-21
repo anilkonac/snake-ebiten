9fe15420f5b95e49385c00f6341bf2f702503297
Anıl Konaç <anil.konac@gmail.com>
2022-01-21 13:36:28 +0300

    Prevent redundant rectangle drawing
    When some part of a rectangle is off-screen, after two parts are drawn,
    an unnecessary full rectangle drawing was called. Prevent this drawing 
    with return statements.

--- rect.go
+++ rect.go
@@ -19,17 +19,21 @@ func (r screenRect) draw(dst *ebiten.Image, color color.Color) {
 	if r.x < 0 { // left part is off the screen
 		screenRect{screenWidth + r.x, r.y, -r.x, r.height}.draw(dst, color) // teleported left part
 		screenRect{0, r.y, r.width + r.x, r.height}.draw(dst, color)        // part that is in the screen
+		return
 	} else if r.x+r.width > screenWidth { // right part is off the screen
 		screenRect{0, r.y, r.x + r.width - screenWidth, r.height}.draw(dst, color) // teleported right part
 		screenRect{r.x, r.y, screenWidth - r.x, r.height}.draw(dst, color)         // part that is in the screen
+		return
 	}
 
 	if r.y < 0 { // upper part is off the screen
 		screenRect{r.x, screenHeight + r.y, r.width, -r.y}.draw(dst, color) // teleported upper part
 		screenRect{r.x, 0, r.width, r.height + r.y}.draw(dst, color)        // part that is in the screen
+		return
 	} else if r.y+r.height > screenHeight { // bottom part is off the screen
 		screenRect{r.x, 0, r.width, r.y + r.height - screenHeight}.draw(dst, color) // teleported right part
 		screenRect{r.x, r.y, r.width, screenHeight - r.y}.draw(dst, color)          // part that is in the screen
+		return
 	}
 
 	// Draw the rectangle at last
