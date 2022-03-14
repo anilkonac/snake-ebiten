3bced4a6f8f79e0efe1d45b53c4fdb873798e67b
Anıl Konaç <anil.konac@gmail.com>
2022-03-15 01:06:22 +0300

    Improve head growth in the vertical axis
    Revise the upward growing formula and add downward growing. These 
    provide a smoother growth on the vertical axis.

--- game/shaders/round.go
+++ game/shaders/round.go
@@ -30,36 +30,28 @@ func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
 
 	posInUnit := vec2(RectSize.x*texCoord.x, RectSize.y*texCoord.y) + RectPosInUnit
 	if IsVertical > 0 {
-		// if TotalSize.y >= TotalSize.x {
 		radius := TotalSize.x / 2
-		// roundCenter1 := vec2(radius)
-		roundCenter2 := vec2(radius, TotalSize.y-radius)
-
-		// distToCenter1 := distance(posInUnit, roundCenter1)
-		distToCenter2 := distance(posInUnit, roundCenter2)
-		multiplier := clamp(TotalSize.y/radius, 0, 1)
 
 		// Top Left Corner
 		if (ShadedCorners[0] > 0) && (posInUnit.x < radius) &&
-			(posInUnit.y < radius) && (posInUnit.y <= theFunc(posInUnit.x, radius, multiplier)) {
+			(posInUnit.y < radius) && (posInUnit.y < growUp(posInUnit.x, radius, TotalSize.y)) {
 			normColor.a = 0
 		}
 		// Bottom Left Corner
 		if (ShadedCorners[1] > 0) && (posInUnit.x < radius) &&
-			(posInUnit.y > (TotalSize.y - radius)) && (distToCenter2 > radius) {
+			(posInUnit.y > (TotalSize.y - radius)) && (posInUnit.y > growDown(posInUnit.x, radius, TotalSize.y)) {
 			normColor.a = 0
 		}
 		// Bottom Right Corner
 		if (ShadedCorners[2] > 0) && (posInUnit.x > radius) &&
-			(posInUnit.y > (TotalSize.y - radius)) && (distToCenter2 > radius) {
+			(posInUnit.y > (TotalSize.y - radius)) && (posInUnit.y > growDown(posInUnit.x, radius, TotalSize.y)) {
 			normColor.a = 0
 		}
 		// Top Right Corner
 		if (ShadedCorners[3] > 0) && (posInUnit.x > radius) &&
-			(posInUnit.y < radius) && (posInUnit.y <= theFunc(posInUnit.x, radius, multiplier)) {
+			(posInUnit.y < radius) && (posInUnit.y < growUp(posInUnit.x, radius, TotalSize.y)) {
 			normColor.a = 0
 		}
-		// }
 
 	} else {
 		// if TotalSize.x >= TotalSize.y {
@@ -70,8 +62,6 @@ func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
 		distToCenter1 := distance(posInUnit, roundCenter1)
 		distToCenter2 := distance(posInUnit, roundCenter2)
 
-		// radius *= clamp((TotalSize.y / TotalSize.x), 0, 1)
-		// radius = min(radius, TotalSize.x)
 		// Top Left Corner
 		if (ShadedCorners[0] > 0) && (posInUnit.x < radius) &&
 			(posInUnit.y < radius) && (distToCenter1 > radius) {
@@ -92,24 +82,30 @@ func Fragment(position vec4, texCoord vec2, color vec4) vec4 {
 			(posInUnit.y < radius) && (distToCenter2 > radius) {
 			normColor.a = 0
 		}
-		// }
 	}
 
 	normColor.rgb *= normColor.a
 	return normColor
 }
 
-func theFunc(x float, radius float, multiplier float) float {
-	// if multiplier == 1 {
-	// 	return radius - sqrt(radius*radius-(x-radius)*(x-radius))
-	// }
-	// return multiplier * ( /*radius -*/ pow(x-radius, 2) / radius)
-
-	// Linear interpolation between squared function and circle function
-	y0 := multiplier * ( /*radius -*/ pow(x-radius, 2) / radius)
+func growUp(x, radius, height float) float {
+	// Linear interpolation between squared function and semicircle function
+	heightMultip := clamp(TotalSize.y/radius, 0, 1)
+	y0 := heightMultip * (pow(x-radius, 2) / radius)
 	x0 := 0.0
 	x1 := 1.0
 	y1 := radius - sqrt(radius*radius-(x-radius)*(x-radius))
 
-	return y0 + (multiplier-x0)*(y1-y0)/(x1-x0)
+	return y0 + (heightMultip-x0)*(y1-y0)/(x1-x0)
+}
+
+func growDown(x, radius, height float) float {
+	// Linear interpolation between squared function and semicircle function
+	heightMultip := clamp(TotalSize.y/radius, 0, 1)
+	y0 := heightMultip * (0.5 - pow(x-radius, 2)/radius)
+	x0 := 0.0
+	x1 := 1.0
+	y1 := height - heightMultip*radius + sqrt(pow(radius, 2)-pow(x-radius, 2))
+
+	return y0 + (heightMultip-x0)*(y1-y0)/(x1-x0)
 }
--- go.mod
+++ go.mod
@@ -7,9 +7,9 @@ require github.com/hajimehoshi/ebiten/v2 v2.2.5
 require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
-	golang.org/x/exp/shiny v0.0.0-20220307080910-a2e15db56b5f // indirect
+	golang.org/x/exp/shiny v0.0.0-20220314205449-43aec2f8a4e7 // indirect
 	golang.org/x/image v0.0.0-20220302094943-723b81ca9867 // indirect
-	golang.org/x/mobile v0.0.0-20220224134551-8a0a1e50732f // indirect
+	golang.org/x/mobile v0.0.0-20220307220422-55113b94f09c // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 )
