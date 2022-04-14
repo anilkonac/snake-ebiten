5795685c9c5e8977c72706fe5cc9f620ec3e857b
Anıl Konaç <anil.konac@gmail.com>
2022-04-15 00:25:36 +0300

    Remove wrong and extra audio.Player creation

--- game/audio.go
+++ game/audio.go
@@ -19,17 +19,15 @@ const sampleRate = 44100
 
 var (
 	audioContext *audio.Context
-	playerMap    map[soundE]*audio.Player
+	audioPlayers map[soundE]*audio.Player
 )
 
 func prepareAudio() {
 	audioContext = audio.NewContext(sampleRate)
-	playerMap = make(map[soundE]*audio.Player, soundTotal)
+	audioPlayers = make(map[soundE]*audio.Player, soundTotal)
 
 	addPlayer(soundEating, sound.Eating)
 	addPlayer(soundSlap, sound.Slap)
-
-	playerMap[soundSlap] = audioContext.NewPlayerFromBytes(sound.Slap)
 }
 
 func addPlayer(sound soundE, src []byte) {
@@ -37,7 +35,7 @@ func addPlayer(sound soundE, src []byte) {
 	if err != nil {
 		panic(err)
 	}
-	playerMap[sound], err = audioContext.NewPlayer(stream)
+	audioPlayers[sound], err = audioContext.NewPlayer(stream)
 	if err != nil {
 		panic(err)
 	}
@@ -48,6 +46,7 @@ func play(sound soundE) {
 		panic("Wrong sound enum")
 	}
 
-	playerMap[sound].Rewind()
-	playerMap[sound].Play()
+	player := audioPlayers[sound]
+	player.Rewind()
+	player.Play()
 }
