f456e88ae061a5d1c8ca7cc59f3fbf7b1ca3067c
Anıl Konaç <anil.konac@gmail.com>
2022-06-12 17:08:22 +0300

    Add the results to a .csv file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	g "github.com/anilkonac/snake-ebiten/game"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,10 +14,20 @@ func main() {
 	ebiten.SetWindowTitle("Ssnake")
 	ebiten.RunGame(g.NewGame())
 
+	f, err := os.OpenFile("results.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fTicks := float64(g.NumTicks)
+	avgTPS := g.TPSSum / fTicks
+	avgFPS := g.FPSSum / fTicks
+	if _, err := f.WriteString(fmt.Sprintf("%d\t\t%f\t%f\t%f\t\t%f\t%f\t%f\n", g.NumTicks, g.TPSMin, g.TPSMax, avgTPS, g.FPSMin, g.FPSMax, avgFPS)); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf("TicksElapsed: %d\n", g.NumTicks)
-	fmt.Printf("TPS | Min: %.2f, Max: %.2f, Avg: %.2f\n", g.TPSMin, g.TPSMax, g.TPSSum/fTicks)
-	fmt.Printf("FPS | Min: %.2f, Max: %.2f, Avg: %.2f\n", g.FPSMin, g.FPSMax, g.FPSSum/fTicks)
+	fmt.Printf("TPS | Min: %.2f, Max: %.2f, Avg: %.2f\n", g.TPSMin, g.TPSMax, avgTPS)
+	fmt.Printf("FPS | Min: %.2f, Max: %.2f, Avg: %.2f\n", g.FPSMin, g.FPSMax, avgFPS)
 }
 
 //go:generate file2byteslice -input game/shader/basic.go -output game/shader/basic_go.go -package shader -var Basic
